c1e922a4d93158dc23d49bafdbcf4579a3e58067
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-08-12 18:21:45 +0300

    base of charts added

---
+++ src/main/java/jsf/ChartsViewBean.java
@@ -0,0 +1,70 @@
+package jsf;
+ 
+import java.io.Serializable;
+ 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+import service.RecordService;
+ 
+public class ChartsViewBean extends AbstractBean implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
+	
+	private static  RecordService instance =  RecordService.getInstance();
+	
+	private LineChartModel areaModel;
+ 
+	public ChartsViewBean() {
+		 createAreaModel();
+	}
+ 
+    public LineChartModel getAreaModel() {
+        return areaModel;
+    }
+     
+    private void createAreaModel() {
+        areaModel = new LineChartModel();
+ 
+        LineChartSeries boys = new LineChartSeries();
+        boys.setFill(true);
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+ 
+        LineChartSeries girls = new LineChartSeries();
+        girls.setFill(true);
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
+ 
+        areaModel.addSeries(boys);
+        areaModel.addSeries(girls);
+         
+        areaModel.setTitle("Area Chart");
+        areaModel.setLegendPosition("ne");
+        areaModel.setStacked(true);
+        areaModel.setShowPointLabels(true);
+         
+        Axis xAxis = new CategoryAxis("Years");
+        areaModel.getAxes().put(AxisType.X, xAxis);
+        Axis yAxis = areaModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(300);
+    }
+
+	public void setAreaModel(LineChartModel areaModel) {
+		this.areaModel = areaModel;
+	}
+     
+}
