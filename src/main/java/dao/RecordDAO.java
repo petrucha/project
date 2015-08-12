c35e94d8fe76e146e8b6b890f44b7daa8be593bf
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-08-12 17:47:54 +0300

    layout for table page

--- src/main/java/dao/RecordDAO.java
+++ src/main/java/dao/RecordDAO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-import entity.Group;
 import entity.Record;
 
 public class RecordDAO extends AbstractDAO<Record> {
