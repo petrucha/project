738948216a701b8f1232820de2c91f5b02c88eaa
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-08-14 20:59:45 +0300

    bar chart added (not configured)

--- src/main/java/jsf/ChartsViewBean.java
+++ src/main/java/jsf/ChartsViewBean.java
@@ -12,7 +12,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.event.SelectEvent;
+import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.DateAxis;
 import org.primefaces.model.chart.LineChartModel;
@@ -26,27 +28,29 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private static RecordService instance = RecordService.getInstance();
-	
-	//Bean variables
-	
+
+	// Bean variables
+
 	private LineChartModel valueModel;
-	
+
 	private PieChartModel averageModel;
-	
+
+	private BarChartModel barModel;
+
 	private String[] selectedDevices;
-	
-    private List<String> devices;
-    
-    private Date startDate = new Date();
-    
-    private Date endDate = new Date();
-    
-    // Constructor
+
+	private List<String> devices;
+
+	private Date startDate = new Date();
+
+	private Date endDate = new Date();
+
+	// Constructor
 
 	public ChartsViewBean() {
 		devices = Arrays.asList(instance.getDevicesArray());
 		// choosing first 5 devices
-		if (devices.size() < 5 ) {
+		if (devices.size() < 5) {
 			selectedDevices = new String[devices.size()];
 		} else {
 			selectedDevices = new String[5];
@@ -57,14 +61,22 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 		// setting a period that starts in first day of last month until now
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.MONTH, -1);
-//		cal.set(Calendar.DATE, 1);
+		// cal.set(Calendar.DATE, 1);
 		startDate = cal.getTime();
 		endDate = new Date();
-		//initializing of filtered data
+		// initializing of filtered data
 		filtersChange();
 	}
-	
+
 	// Getters and setters
+
+	public BarChartModel getBarModel() {
+		return barModel;
+	}
+
+	public void setBarModel(BarChartModel barModel) {
+		this.barModel = barModel;
+	}
 
 	public LineChartModel getValueModel() {
 		return valueModel;
@@ -73,7 +85,7 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 	public void setValueModel(LineChartModel valueModel) {
 		this.valueModel = valueModel;
 	}
-	
+
 	public String[] getSelectedDevices() {
 		return selectedDevices;
 	}
@@ -105,7 +117,7 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-	
+
 	public PieChartModel getAverageModel() {
 		return averageModel;
 	}
@@ -115,9 +127,36 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 	}
 
 	// Chart methods
+	
+	private void createValueModels(){
+		List<Record[]> recordArrays = instance.getRecordsByDevicesAndTime(selectedDevices, startDate.getTime(),
+				endDate.getTime());
+		createLineModel(recordArrays);
+		createBarModel(recordArrays);
+	}
 
-	private void createValueModel() {
-		List<Record[]> recordArrays = instance.getRecordsByDevicesAndTime(selectedDevices, startDate.getTime(), endDate.getTime());
+	private void createBarModel(List<Record[]> recordArrays) {
+		BarChartModel model = new BarChartModel();
+		
+		for (Record[] recs : recordArrays) {
+			if (recs.length == 0) {
+				System.out.println("NO DATA FOR THE PERIOD!");
+			} else {
+				barModel = initBarModel(recs, model);
+			}
+		}
+
+		barModel.setTitle("Bar chart of last records");
+		barModel.setAnimate(true);
+		barModel.setLegendPosition("ne");
+
+		barModel.getAxis(AxisType.Y).setLabel("Temperature C\u00b0");
+
+		Axis xAxis = barModel.getAxis(AxisType.X);
+		xAxis.setLabel("Days");
+	}
+
+	private void createLineModel(List<Record[]> recordArrays) {
 		LineChartModel model = new LineChartModel();
 
 		for (Record[] recs : recordArrays) {
@@ -131,22 +170,35 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 		valueModel.setTitle("Temperature Chart");
 		valueModel.setAnimate(true);
 		valueModel.setLegendPosition("e");
-		valueModel.getAxis(AxisType.Y).setLabel("Temperature C\u00b0");
 		
-        DateAxis axisX = new DateAxis("Dates");
-        String tomorrow = printTomorrow();
-        //set tomorrow as max day
-        axisX.setTickAngle(-50);
-        axisX.setMax(tomorrow);
-        axisX.setTickFormat("%b %#d | %H:%M");
-         
-        valueModel.getAxes().put(AxisType.X, axisX);
+		valueModel.getAxis(AxisType.Y).setLabel("Temperature C\u00b0");
+		DateAxis axisX = new DateAxis("Dates");
+		String tomorrow = printTomorrow();
+		// set tomorrow as max day
+		axisX.setTickAngle(-50);
+		axisX.setMax(tomorrow);
+		axisX.setTickFormat("%b %#d | %H:%M");
+		valueModel.getAxes().put(AxisType.X, axisX);
 	}
 
-	private LineChartModel initCategoryModel(Record[] records, LineChartModel model) { 
+	private void createAverageModel() {
+		HashMap<String, Double> averages = instance.getFilteredAverages(selectedDevices, startDate.getTime(),
+				endDate.getTime());
+		averageModel = new PieChartModel();
+
+		for (String key : averages.keySet()) {
+			averageModel.set(key, averages.get(key));
+		}
+
+		averageModel.setTitle("Average per device");
+		averageModel.setLegendPosition("w");
+		averageModel.setShowDataLabels(true);
+	}
+
+	private LineChartModel initCategoryModel(Record[] records, LineChartModel model) {
 		ChartSeries values = new ChartSeries();
 		values.setLabel(records[0].getDevice());
-														
+
 		// creating value data for chart series
 		for (Record rec : records) {
 			String date = new SimpleDateFormat("yyyy-MM-dd hh:mm").format(rec.getTimestamp());
@@ -158,43 +210,46 @@ public class ChartsViewBean extends AbstractBean implements Serializable {
 		return model;
 	}
 	
-	private void createAverageModel() {
-		HashMap<String, Double> averages = instance.getFilteredAverages(selectedDevices, startDate.getTime(), endDate.getTime());
-        averageModel = new PieChartModel();
-        
-        for (String key : averages.keySet()) {
-        	averageModel.set(key, averages.get(key));
-    	}
-         
-        averageModel.setTitle("Average per device");
-        averageModel.setLegendPosition("w");
-        averageModel.setShowDataLabels(true);
-    }
-	
-	//filter methods
-	
+	private BarChartModel initBarModel(Record[] records, BarChartModel model) {
+		ChartSeries values = new ChartSeries();
+		values.setLabel(records[0].getDevice());
+
+		// creating value data for chart series
+		for (Record rec : records) {
+			String date = new SimpleDateFormat("yyyy-MM-dd hh:mm").format(rec.getTimestamp());
+			// Double quan = Double.parseDouble(rec.getQuantity());
+			values.set(date, rec.getValue());
+		}
+		model.addSeries(values);
+
+		return model;
+	}
+
+	// filter methods
+
 	public void filtersChange() {
 		System.out.println(startDate);
 		System.out.println(endDate);
-		createValueModel();
+		createValueModels();
 		createAverageModel();
 	}
-	
+
 	public void onDateSelect(SelectEvent event) {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
-    }
-	
-	//utility methods
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		facesContext.addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+	}
+
+	// utility methods
 
 	private String printTomorrow() {
 		Date dt = new Date();
-        Calendar c = Calendar.getInstance(); 
-        c.setTime(dt); 
-        c.add(Calendar.DATE, 1);
-        dt = c.getTime();
-        String tomorrow = new SimpleDateFormat("yyyy-MM-dd").format(dt);
+		Calendar c = Calendar.getInstance();
+		c.setTime(dt);
+		c.add(Calendar.DATE, 1);
+		dt = c.getTime();
+		String tomorrow = new SimpleDateFormat("yyyy-MM-dd").format(dt);
 		return tomorrow;
 	}
 
