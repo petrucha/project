e69b3d90618ab5476ccd6365a5ca92d992bfc543
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-08-24 17:38:14 +0300

    logged GroupDAO and GroupService

--- src/main/java/dao/GroupDAO.java
+++ src/main/java/dao/GroupDAO.java
@@ -1,21 +1,31 @@
 package dao;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
 import entity.Group;
 
 public class GroupDAO extends AbstractDAO<Group> {
+	
+	private static final Logger LOG = Logger.getLogger(GroupDAO.class);
 
 	public GroupDAO() {
 		super(Group.class);
 	}
 
+	/**
+	 * @param groupname
+	 * @return group by group name
+	 */
 	public Group getGroupByName(final String groupname) {
 		Session hibernateSession = this.getSession();
 		String hql = "SELECT g FROM Group g WHERE g.groupname = :groupname";
-		Query query = hibernateSession.createQuery(hql).setParameter(
-				"groupname", groupname);
+		LOG.trace("Creating a query: " + hql);
+		Query query = hibernateSession.createQuery(hql);
+		LOG.trace("Setting a param \"groupname\"=" + groupname);
+		query.setParameter("groupname", groupname);
+		
 		return this.findOne(query);
 	}
 
--- src/main/java/service/GroupService.java
+++ src/main/java/service/GroupService.java
@@ -2,6 +2,7 @@ package service;
 
 import java.io.Serializable;
 
+import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 
 import util.HibernateUtil;
@@ -11,7 +12,11 @@ import entity.Group;
 public class GroupService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOG = Logger.getLogger(GroupService.class);
+	
 	private static GroupDAO groupDAO = new GroupDAO();
+	
 	private static GroupService instance = null;
 
 	public static GroupService getInstance() {
@@ -22,38 +27,58 @@ public class GroupService implements Serializable {
 		return instance;
 	}
 	
+	/**
+	 * @param group
+	 * @return true if a group is successfully added, else false
+	 */
 	public boolean addGroup(Group group) {
 		try {
 			HibernateUtil.beginTransaction();
+			LOG.debug("Saving a group: " + group.toString());
 			groupDAO.save(group);
 			HibernateUtil.commitTransaction();
 		} catch (HibernateException ex) {
-			System.out.println("Error: addGroup(" + group + ")");
+			LOG.error("Failed to create a group: " + group.toString());
+			LOG.error(ex.getCause());
+			HibernateUtil.rollbackTransaction();
 		}
 		
 		return true;
 	}
 
+	/**
+	 * @param groupname
+	 * @return a group by it's name 
+	 */
 	public Group getGroupByName(final String groupname) {
 		Group group = null;
 		try {
 			HibernateUtil.beginTransaction();
+			LOG.debug("Getting a group with name: " + groupname);
 			group = groupDAO.getGroupByName(groupname);
 			HibernateUtil.commitTransaction();
 		} catch (HibernateException ex) {
-			System.out.println("Error: getGroupByName()");
+			LOG.error("Failed to get a group with name: " + groupname);
+			LOG.error(ex.getCause());
 		}
 		
 		return group;
 	}
 	
+	/**
+	 * @param group
+	 * @return true if a group is successfully deleted, else false
+	 */
 	public boolean deleteGroup(Group group) {
 		try {
 			HibernateUtil.beginTransaction();
+			LOG.debug("Deleting a group: " + group.toString());
 			groupDAO.delete(group);
 			HibernateUtil.commitTransaction();
 		} catch (HibernateException ex) {
-			System.out.println("Error: deleteGroup(" + group + ")");
+			LOG.error("Failed to delete a group: " + group.toString());
+			LOG.error(ex.getCause());
+			HibernateUtil.rollbackTransaction();
 		}
 		
 		return true;
